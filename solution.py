07b95cd46d4a0eabaff6b2d8211aeca51b6f4c2f
hitemth <ivashkina.em@phystech.edu>
2023-06-07 23:55:10 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import numpy as np
 
+import math
+from scipy.stats import norm
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 156287560 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    control_conversion = x_success / x_cnt
+    test_conversion = y_success / y_cnt
+    se = math.sqrt(control_conversion * (1 - control_conversion) / x_cnt + test_conversion * (1 - test_conversion) / y_cnt)
+    z = (test_conversion - control_conversion) / se
+    p_value = norm.sf(abs(z))
+    return p_value <0.05 
